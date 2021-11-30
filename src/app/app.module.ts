0a544618d5ee578ab836c9b40fe25a633fb33e90
joseRiver39 <joseantonior4@hotmail.es>
2021-11-30 17:48:54 -0500

    instalacion de bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonasComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule,
